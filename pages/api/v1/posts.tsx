8f46a8c29e8a55a3e894a56ff55497e6b9533d36
liuyidiao <1070692044@qq.com>
2022-04-24 21:06:07 +0800

    读取markdown

--- pages/api/v1/posts.tsx
+++ pages/api/v1/posts.tsx
@@ -1,4 +1,12 @@
 import {NextApiHandler, NextApiRequest, NextApiResponse} from "next";
+import fs,{promises as fsPromise} from 'fs'
+import path from "path/posix";
+
+//读取文件
+const getPosts = async ()=>{
+    const markdowDir = path.join(process.cwd(),'markdown')
+    const filename = await fsPromise.readdir('markdowDir')
+}
 
 const Posts:NextApiHandler = (req:NextApiRequest,res:NextApiResponse)=>{
     res.statusCode = 200
