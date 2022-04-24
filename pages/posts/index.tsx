a9de429d8184a20083bcab15d3b77f281ed7e626
liuyidiao <1070692044@qq.com>
2022-04-24 21:44:08 +0800

    isLoading

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -2,17 +2,30 @@ import NextPage from 'next'
 import React, {useEffect, useState} from 'react'
 import axios from "axios";
 
+type Post = {
+    id: string;
+    date: string;
+    title: string;
+}
 // @ts-ignore
 const postsIndex: NextPage = () => {
-    const [posts,setPosts] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [posts, setPosts] = useState<Post[]>([])
     useEffect(() => {
+        setIsLoading(true)
         axios.get('api/v1/posts').then(x => {
             console.log('xxx', x)
+        }, () => {
+            setIsLoading(false)
         })
     }, [])
     return (
         <div>
-            posts Index
+            <h1>文章</h1>
+            {isLoading?<div>加载中</div>:
+            posts.map(p => <div key={p.id}>
+                {p.id}
+            </div>)}
         </div>
     )
 }
