faba53bc44eaff2db57a5ac17b70a585ed7cab16
liuyidiao <1070692044@qq.com>
2022-04-24 21:24:20 +0800

    获取文件的数据

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -1,10 +1,19 @@
 //读取文件
 import path from "path/posix";
 import {promises as fsPromise} from "fs";
+import * as fs from "fs";
+import matter from "gray-matter";
 
 export const getPosts = async ()=>{
     const markdowDir = path.join(process.cwd(),'markdown')
     const filename = await fsPromise.readdir('markdowDir')
+    const x =filename.map(filename=>{
+        const id =filename.replace(/\.md$/g,'')
+        const fullPath = path.join(markdowDir,filename)
+        const text = fs.readFileSync(fullPath,"utf-8")
+        const {data:{title,data},content} = matter(text)
+        return{ id,title,data}
+    })
     console.log('filename',filename)
     return filename
 }
